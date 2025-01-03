ec6c445baeafd276aa0866e6c57d157ecb3b9154
LuigiAmodio08 <luigiamodio23@gmail.com>
2025-01-03 14:14:35 +0100

    Update main.py

--- Gestione BnB/main.py
+++ Gestione BnB/main.py
@@ -1,10 +1,10 @@
 # main.py
 from app import BnBApp
-from bnb import BnB  # Importa il modulo bnb contenente la classe BnB
+from bnb import BnB  
 import tkinter as tk
 
 if __name__ == "__main__":
-    root = tk.Tk()          # Crea la finestra principale
-    bnb = BnB()             # Crea l'istanza del BnB
-    app = BnBApp(root, bnb) # Avvia l'app
-    root.mainloop()         # Avvia il ciclo dell'interfaccia grafica
+    root = tk.Tk()         
+    bnb = BnB()            
+    app = BnBApp(root, bnb)
+    root.mainloop()         
